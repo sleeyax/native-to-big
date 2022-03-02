297ae70e68a42472efe887ce936805c3743a25ee
Sleeyax <yourd3veloper@gmail.com>
2022-03-03 00:44:32 +0100

    Create basic CLI app

--- bin/main.ts
+++ bin/main.ts
@@ -1,1 +1,16 @@
-// TODO: create CLI app
+#!/usr/bin/env node
+
+import { program } from 'commander';
+import { name, version, description } from '../package.json';
+import convert from '../src/converter';
+
+program
+  .name(name)
+  .version(version)
+  .description(description)
+  .option('-s, --sources <glob pattern or source code>', 'source file(s) or code to convert')
+  .parse();
+
+const options: {sources: string} = program.opts();
+
+console.log(convert(options.sources));
