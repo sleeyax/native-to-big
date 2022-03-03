97c0ccad4c14756ac4d06de3daf0f48fb1d2274d
Sleeyax <yourd3veloper@gmail.com>
2022-03-03 01:10:06 +0100

    Group tests, add combined test

--- src/converter.test.ts
+++ src/converter.test.ts
@@ -1,25 +1,33 @@
 import convert from './converter';
 
-test('it should convert additions', () => {
-  const input = '1 + 2 + 3 + 4 + 5';
-  const output = 'Big(1).plus(2).plus(3).plus(4).plus(5)';
-  expect(convert(input)).toBe(output);
-});
+describe('Convert simple mathematical expressions', () => {
+  test('it should convert additions', () => {
+    const input = '1 + 2 + 3 + 4 + 5';
+    const output = 'Big(1).plus(2).plus(3).plus(4).plus(5)';
+    expect(convert(input)).toBe(output);
+  });
 
-test('it should convert substractions', () => {
-  const input = '5 - 4 - 3 - 2 - 1';
-  const output = 'Big(5).minus(4).minus(3).minus(2).minus(1)';
-  expect(convert(input)).toBe(output);
-});
+  test('it should convert substractions', () => {
+    const input = '5 - 4 - 3 - 2 - 1';
+    const output = 'Big(5).minus(4).minus(3).minus(2).minus(1)';
+    expect(convert(input)).toBe(output);
+  });
 
-test('it should convert multiplications', () => {
-  const input = '1 * 2 * 3 * 4 * 5';
-  const output = 'Big(1).times(2).times(3).times(4).times(5)';
-  expect(convert(input)).toBe(output);
-});
+  test('it should convert multiplications', () => {
+    const input = '1 * 2 * 3 * 4 * 5';
+    const output = 'Big(1).times(2).times(3).times(4).times(5)';
+    expect(convert(input)).toBe(output);
+  });
 
-test('it should convert divisions', () => {
-  const input = '5 / 4 / 3 / 2 / 1';
-  const output = 'Big(5).div(4).div(3).div(2).div(1)';
-  expect(convert(input)).toBe(output);
+  test('it should convert divisions', () => {
+    const input = '5 / 4 / 3 / 2 / 1';
+    const output = 'Big(5).div(4).div(3).div(2).div(1)';
+    expect(convert(input)).toBe(output);
+  });
+
+  test('it should perform all simple expressions combined', () => {
+    const input = '1 + 2 - 3 * 4 / 5';
+    const output = 'Big(1).plus(2).minus(Big(3).times(4).div(5))';
+    expect(convert(input)).toBe(output);
+  });
 });
